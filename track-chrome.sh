e7459d786a373d2424d3eb199888109008712e1c
NavyTitanium <33707347+NavyTitanium@users.noreply.github.com>
2018-01-15 13:43:52 -0500

    Update track-chrome.sh

--- track-chrome.sh
+++ track-chrome.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 nb_try=3;
+log_duplicate=false
 logfile="$DIR/logs-urls-chrome.txt";
 user_agent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+current_try=1;
+
+function is_duplicate() {
+	URL=$1
+	FILE=$2
+	if grep -q $URL $FILE ; then
+		return 1
+	else
+		return 0
+	fi
+}
 
 function create_log_file() {
 
@@ -18,16 +30,20 @@ function create_log_file() {
 }
 
 function get_url() {
-        if [ "$nb_try" -ge 1 ] ; then
-                echo "try" $nb_try;
+        if [ "$current_try" -le "$nb_try" ] ; then
+                echo "try" $current_try;
                 url=$(php eitest.php "$user_agent" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*")
                 if [ ${#url} -ge 2 ];then
                         echo "Success" ;
-                        timestamp=`date "+%Y%m%d_%H-%M-%S"`
-                        echo $url ':' $timestamp >> $logfile
-                        exit
+			if $log_duplicate || (is_duplicate $url $logfile) ; then 
+                        	timestamp=`date "+%Y%m%d_%H-%M-%S"`
+                        	echo $url ':' $timestamp >> $logfile
+                        	exit
+			else
+				echo "Duplicate URL" ;				
+			fi
                 else
-                   nb_try=$((nb_try - 1));
+                   let current_try++;
                    get_url
                 fi
         fi
